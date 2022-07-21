90717d52a2b1ed57a32788570dd1f7e7f9bf53e4
Gift Masha <gftman24@gmail.com>
2022-07-21 08:56:31 +0300

    printing the letters of a string

---
+++ strings.py
@@ -0,0 +1,7 @@
+#in python it is possible to print out each letter of a string#
+#a string is a sequence of letters#
+s="python"
+print (s [0])
+#this prints the first letter p#
+print(s [3])
+#this prints the 3rd letter t 
